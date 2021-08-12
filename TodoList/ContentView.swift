1f11c1c51a0330185b6af800c11b4dc0f2a7fe6c
Cristopher Escorcia <escorciacristopher@gmail.com>
2021-08-12 16:51:51 -0500

    feat: adds the content view file

---
+++ TodoList/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  TodoList
+//
+//  Created by Cristopher Escorcia on 10/08/21.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    var body: some View {
+       Home()
+    }
+}
